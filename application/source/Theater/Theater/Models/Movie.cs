a90536c4876be3408cd8c823e5237f891abe0a17
Atiq Ahammed <atiqahammedshamim@gmail.com>
2020-04-09 15:55:08 +0600

    initial application has run

--- application/source/Theater/Theater/Models/Movie.cs
+++ application/source/Theater/Theater/Models/Movie.cs
@@ -8,6 +8,6 @@ namespace Theater.Models
     public class Movie
     {
         public int Id { get; set; }
-        public string Namw { get; set; }
+        public string Name { get; set; }
     }
 }
