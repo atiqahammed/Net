e69ea8a7f958dfb895b66843c6a142eb418d3baf
Atiq Ahammed <atiqahammedshamim@gmail.com>
2020-04-09 14:23:48 +0600

    project started with a new class name Movie.cs

---
+++ application/source/Theater/Theater/Models/Movie.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Theater.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Namw { get; set; }
+    }
+}
