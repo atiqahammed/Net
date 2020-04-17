56c149248132a53f19b9401a3475f45019702af5
Atiq Ahammed <atiqahammedshamim@gmail.com>
2020-04-18 01:44:59 +0600

    start the create page with ISBN in book object

--- BookList/BookList/Pages/BookListPage/Create.cshtml.cs
+++ BookList/BookList/Pages/BookListPage/Create.cshtml.cs
@@ -4,11 +4,20 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using BookList.Model;
 
 namespace BookList.Pages.BookListPage
 {
     public class CreateModel : PageModel
     {
+        private readonly ApplicationDBContext _db;
+
+        public CreateModel(ApplicationDBContext _db)
+        {
+            this._db = _db;
+        }
+
+        public Book Book { get; set; }
         public void OnGet()
         {
 
