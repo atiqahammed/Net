1eb22d075e4acb43e45591145e9a44ae62d35a8a
Atiq Ahammed <atiqahammedshamim@gmail.com>
2020-04-17 19:35:58 +0600

    create new book application

---
+++ BookList/BookList/Pages/BookListPage/Index.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookList.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookList.Pages.BookListPage
+{
+    public class IndexModel : PageModel
+    {
+
+        private readonly ApplicationDBContext _db;
+        public IEnumerable<Book> Books;
+        public IndexModel(ApplicationDBContext _db)
+        {
+            this._db = _db;
+        }
+
+        public async Task OnGet()
+        {
+            Books = await this._db.Book.ToListAsync();
+        }
+    }
+}
