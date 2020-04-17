d1e32ab98949baa34b2b087b74e8f20876e6eec5
Atiq Ahammed <atiqahammedshamim@gmail.com>
2020-04-18 02:20:46 +0600

    post method implementation has been complete

--- BookList/BookList/Pages/BookListPage/Create.cshtml.cs
+++ BookList/BookList/Pages/BookListPage/Create.cshtml.cs
@@ -16,11 +16,25 @@ namespace BookList.Pages.BookListPage
         {
             this._db = _db;
         }
-
+        [BindProperty]
         public Book Book { get; set; }
         public void OnGet()
         {
 
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            if(ModelState.IsValid)
+            {
+                await _db.AddAsync(Book);
+                await _db.SaveChangesAsync();
+                return RedirectToPage("Index");
+            } 
+            else
+            {
+                return Page();
+            }
         }
     }
 }
