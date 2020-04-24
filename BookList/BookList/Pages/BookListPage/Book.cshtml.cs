245da9f6582feb01aedfce1ec3ccc0796b79c761
Atiq Ahammed <atiqahammedshamim@gmail.com>
2020-04-25 01:02:01 +0600

    all updated

--- BookList/BookList/Pages/BookListPage/Book.cshtml.cs
+++ BookList/BookList/Pages/BookListPage/Book.cshtml.cs
@@ -11,6 +11,7 @@ namespace BookList.Pages.BookListPage
     public class CreateModel : PageModel
     {
         private readonly ApplicationDBContext _db;
+        public bool IsUpdating = false;
 
         public CreateModel(ApplicationDBContext _db)
         {
@@ -18,19 +19,38 @@ namespace BookList.Pages.BookListPage
         }
         [BindProperty]
         public Book Book { get; set; }
-        public void OnGet()
+        public async Task OnGet(long Id)
         {
-
+            Book = await _db.Book.FindAsync(Id);
+            if(Book != null)
+            {
+                IsUpdating = true;
+            }
+            
         }
 
         public async Task<IActionResult> OnPost()
         {
-            if(ModelState.IsValid)
+
+
+
+            if (ModelState.IsValid && !IsUpdating)
             {
                 await _db.AddAsync(Book);
                 await _db.SaveChangesAsync();
                 return RedirectToPage("Index");
-            } 
+            }
+            else if(ModelState.IsValid && IsUpdating)
+            {
+                var BookFromDB = await _db.Book.FindAsync(Book.Id);
+                BookFromDB.Name = Book.Name;
+                BookFromDB.Author = Book.Author;
+                BookFromDB.ISBN = Book.ISBN;
+
+                Book = null;
+                await _db.SaveChangesAsync();
+                return RedirectToPage("Index");
+            }
             else
             {
                 return Page();
