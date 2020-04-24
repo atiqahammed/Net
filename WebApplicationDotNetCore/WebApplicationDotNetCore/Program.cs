080585ac73027a0a66165df4f5af1ca322f962b6
Atiq Ahammed <atiqahammedshamim@gmail.com>
2020-04-25 01:51:29 +0600

    added another project

--- WebApplicationDotNetCore/WebApplicationDotNetCore/Program.cs
+++ WebApplicationDotNetCore/WebApplicationDotNetCore/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace aspnetcoreapp
+namespace WebApplicationDotNetCore
 {
     public class Program
     {
@@ -18,11 +18,6 @@ namespace aspnetcoreapp
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureLogging(logging =>
-                {
-                    logging.ClearProviders();
-                    logging.AddConsole();
-                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
